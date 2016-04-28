ed7084e18b3e29d19c3f90a1e26472b486b49429
inwon5554 <inwon5554@gmail.com>
2016-04-28 14:47:56 +0900

    메일 수정

--- emt/src/main/java/emt/emt/grant/service/GrantServiceImpl.java
+++ emt/src/main/java/emt/emt/grant/service/GrantServiceImpl.java
@@ -7,6 +7,7 @@ import javax.mail.internet.MimeMessage.RecipientType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,11 +24,12 @@ public class GrantServiceImpl implements GrantService{
 	public void sendAuthMail(User user) {
 		MimeMessage message = mailSender.createMimeMessage();
 			try {
-				message.setSubject("[emt]"+user.getUserId()+"님 회원가입 승인 메일입니다.", "UTF-8");
-				message.setText("<a href='http://localhost/emt/grantAuth?userId="+user.getUserId() + "'>가입승인링크</a>");
-				message.setFrom(new InternetAddress("inwon5554@gmail.com"));
-				message.addRecipient(RecipientType.TO,
-						new InternetAddress(user.getEmail()));
+				MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+				messageHelper.setSubject("[emt]"+user.getUserId()+"님 회원가입 승인 메일입니다.");
+				String htmlContent ="<a href='http://localhost/emt/grantAuth?userId="+user.getUserId() + "'>가입승인링크</a>";
+				messageHelper.setText(htmlContent, true);
+				messageHelper.setFrom(new InternetAddress("inwon5554@gmail.com"));
+				messageHelper.setTo(new InternetAddress(user.getEmail()));
 				mailSender.send(message);
 			} catch (MessagingException e) {
 				return;
