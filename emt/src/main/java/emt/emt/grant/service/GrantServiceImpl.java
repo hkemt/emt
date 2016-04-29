dccbb148c749e7b48ef4ac92cf0f2e5e049495a2
inwon5554 <inwon5554@gmail.com>
2016-04-29 16:03:12 +0900

    grantServiceImpl import 부분 수정

--- emt/src/main/java/emt/emt/grant/service/GrantServiceImpl.java
+++ emt/src/main/java/emt/emt/grant/service/GrantServiceImpl.java
@@ -3,7 +3,6 @@ package emt.emt.grant.service;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMessage.RecipientType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
