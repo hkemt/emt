44e4bcc0bbe5cd5058a12c4e60ac53f40f26240b
inwon5554 <inwon5554@gmail.com>
2016-04-29 16:06:58 +0900

    serverIp 프로퍼티 파일로

--- emt/src/main/java/emt/emt/grant/service/GrantServiceImpl.java
+++ emt/src/main/java/emt/emt/grant/service/GrantServiceImpl.java
@@ -5,6 +5,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,14 @@ public class GrantServiceImpl implements GrantService{
 
 	@Autowired private GrantDao grantDao;
 	@Autowired private JavaMailSender mailSender;
-	
+	@Value("${serverIp}") private String serverIp;
 	@Override
 	public void sendAuthMail(User user) {
 		MimeMessage message = mailSender.createMimeMessage();
 			try {
 				MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 				messageHelper.setSubject("[emt]"+user.getUserId()+"님 회원가입 승인 메일입니다.");
-				String htmlContent ="<a href='http://localhost/emt/grantAuth?uuid="+user.getUuid() + "'>가입승인링크</a>";
+				String htmlContent ="<a href='http://"+serverIp+"/emt/grantAuth?uuid="+user.getUuid() + "'>가입승인링크</a>";
 				messageHelper.setText(htmlContent, true);
 				messageHelper.setFrom(new InternetAddress("inwon5554@gmail.com"));
 				messageHelper.setTo(new InternetAddress(user.getEmail()));
